30d3af6f7b29014f5ce38d6c9d6301524831bf25
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-03-07 09:43:23 +0600

    sign up success

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+function checkUserInfo(){
+    const signBtn = document.getElementById("signBtn");
+    const inputUser = document.getElementById("inputUser").value;
+    const inputPass = document.getElementById("inputPass").value;
+    if (inputUser == 'admin' && inputPass == 'admin123') {
+        signBtn.removeAttribute('disabled');
+    } else {
+        signBtn.setAttribute('disabled', true);
+    }
+}
+document.getElementById('signBtn').addEventListener('click',()=>{
+    alert('Login Successful!!')
+    window.location.assign('issue.html')
+})
+document.getElementById('inputPass').addEventListener('keyup',checkUserInfo);
+document.getElementById('inputUser').addEventListener('keyup',checkUserInfo);
+
